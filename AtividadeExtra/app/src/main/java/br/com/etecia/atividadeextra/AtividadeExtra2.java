8381775e9095794c6d4941422e244563d393edbd
Lucas <lucas.oliva2306@gmail.com>
2021-10-06 20:16:04 -0300

    AtividadeExtra

---
+++ AtividadeExtra/app/src/main/java/br/com/etecia/atividadeextra/AtividadeExtra2.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atividadeextra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtividadeExtra2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_extra2);
+    }
+}
